411d4d305e8e35a67b5a5c86b561f833daa80e93
Akshada Jadhav <92100928+akshadajadhav01@users.noreply.github.com>
2025-05-08 13:16:45 +0530

    changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,9 +13,10 @@ import OutboundOutlinedIcon from "@mui/icons-material/OutboundOutlined";
 
 // components
 import { CustomButton } from "./StyledButton";
+import Cards from "./Cards";
 
 // constant
-import { cards, projects } from "./constant";
+import { projects } from "./constant";
 
 const Projects = () => {
   return (
@@ -229,83 +230,7 @@ const Projects = () => {
           <Typography variant="h6" sx={{ mt: 1 }}>
             Here form their Point Of View
           </Typography>{" "}
-          <Stack
-            direction="row"
-            flexWrap="wrap"
-            spacing={2}
-            sx={{ mt: { lg: 5, md: 3 } }}
-          >
-            {cards?.map((card, index) => (
-              <Box
-                key={index}
-                sx={{
-                  flex: {
-                    xs: "1 1 100%",
-                    md: "1 1 48%",
-                    lg: "1 1 31%",
-                  },
-                  minWidth: { sm: "250px" },
-                }}
-              >
-                <Card
-                  elevation={0}
-                  sx={{
-                    cursor: "pointer",
-                    width: "100%",
-                    borderRadius: 6,
-                    pl: 2,
-                    bgcolor: "rgba(21, 21, 21, 0.4)",
-                    color: "white",
-                    backdropFilter: "blur(20px)",
-                  }}
-                >
-                  <CardContent>
-                    <Box display="flex" flexDirection="row" gap={1} mt={0.5}>
-                      <Box
-                        display="flex"
-                        flexDirection="column"
-                        gap={1}
-                        mt={0.5}
-                        alignItems="flex-start"
-                      >
-                        <Box display="flex" flexDirection="row" gap={3} mb={2}>
-                          <Box
-                            component="img"
-                            src={card.image}
-                            alt="Image"
-                            sx={{
-                              width: 50,
-                              height: 50,
-                              borderRadius: "50%",
-                              objectFit: "cover",
-                            }}
-                          />
-                          <Box display={"flex"} flexDirection="column">
-                            <Typography variant="h6">{card.name}</Typography>
-                            <Typography variant="body2">
-                              {card.position}
-                            </Typography>
-                          </Box>
-                        </Box>
-
-                        <Typography variant="subtitle1" maxWidth={330}>
-                          {card.description}
-                        </Typography>
-                      </Box>
-                      <Box display={"flex"} justifyContent={"flex-end"}>
-                        <Box
-                          component="img"
-                          src={card.comma}
-                          alt="Image"
-                          sx={{ width: 50, height: 50, pb: 0 }}
-                        />
-                      </Box>
-                    </Box>
-                  </CardContent>
-                </Card>
-              </Box>
-            ))}
-          </Stack>
+          <Cards />
         </Box>{" "}
       </Box>
     </>
